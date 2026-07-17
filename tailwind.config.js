2ee26e013dbbecae8d6cbcdfa3aa4727e90b8442
Albert <ksk836684@gmail.com>
2026-07-17 14:13:18 +0400

    feat: global colors vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange-main': '#d7452c',
+        'white-main': '#fffee9',
+        'black-main': '#181818',
+        'gray-main': '#999999',
+      },
+
+      fontFamily: {
+        bebas: ['BebasNeue'],
+        futura: ['FuturaPT'],
+      },
+    },
   },
+
   plugins: [],
 };
